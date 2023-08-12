90f27d9a98e44dc924cda0e85eba3062b4ab0764
Юрій Войтюк <voutuk@icloud.com>
2023-08-12 22:21:35 +0300

    :)

---
+++ wp.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+# Set up error handling and exit on failure
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Random data generation
+pass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
+rand=$RANDOM
+
+# Update package list
+sudo apt update
+
+# Install Apache2 MySQL PHP and required modules
+sudo apt install apache2 mysql-server php libapache2-mod-php php-mysql git -y
+
+# Restart Apache2 service
+sudo systemctl restart apache2.service
+
+# Create MySQL database for WordPress
+sudo mysql -u root <<EOF
+CREATE DATABASE wordpress_$rand;
+CREATE USER 'wp_$rand'@'localhost' IDENTIFIED BY '$pass';
+GRANT ALL PRIVILEGES ON wordpress_$rand.* TO 'wp_$rand'@'localhost';
+FLUSH PRIVILEGES;
+EOF
+
+echo -e "\033[44mЯкщо файл не завантажується спробуйте увімкнути VPN\nIf the file does not download, try to turn on the VPN\033[0m"
+
+# Download and extract WordPress files
+mkdir /tmp/wp_$rand && cd /tmp/wp_$rand
+curl -O https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz
+
+# Copy WordPress files to Apache2 web root directory
+sudo cp -r /tmp/wp_$rand/wordpress/* /var/www/html/
+rm -r /tmp/wp_$rand
+
+# Set permissions for WordPress files and directories
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+
+# Create WordPress configuration file from sample file and set database details
+cd /var/www/html/
+sudo mv wp-config-sample.php wp-config.php
+sudo sed -i "/DB_NAME/s/'[^']*'/'wordpress_$rand'/2" wp-config.php
+sudo sed -i "/DB_USER/s/'[^']*'/'wp_$rand'/2" wp-config.php
+sudo sed -i "/DB_PASSWORD/s/'[^']*'/'$pass'/2" wp-config.php
+sudo rm index.html
+
+
+echo -e "\n=====================\nDB_NAME: wordpress_$rand\nDB_USER: wp_$rand\nDB_PASSWORD: $pass\n=====================\n"
+
---
+++ zab.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+# Set up error handling and exit on failure
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Data generation
+pass=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)
+rand=$RANDOM
+ub=$(lsb_release -rs)
+
+# Download Zabbix repository package
+wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu${ub}_all.deb
+
+# Install Zabbix repository package
+sudo dpkg -i zabbix-release_6.0-4+ubuntu${ub}_all.deb
+rm zabbix-release_6.0-4+ubuntu${ub}_all.deb
+
+# Update the package list
+sudo apt update
+
+# Install Zabbix packages, MySQL, and dependencies
+sudo apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql-server pv -y
+
+# Create a database and user for Zabbix in MySQL
+echo "Enter system password"
+sudo mysql -uroot -p <<EOF
+create database zabbix_$rand character set utf8mb4 collate utf8mb4_bin;
+create user user_$rand@localhost identified by '$pass';
+grant all privileges on zabbix_$rand.* to user_$rand@localhost;
+set global log_bin_trust_function_creators = 1;
+EOF
+
+# Load Zabbix database structure
+
+echo "Password: $pass\nThe download will complete at 37.0MiB"
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | pv | mysql --default-character-set=utf8mb4 -uuser_$rand -p zabbix_$rand
+
+# Revert the log_bin_trust_function_creators property
+echo "Enter system password"
+sudo mysql -uroot -p <<EOF
+set global log_bin_trust_function_creators = 0;
+EOF
+
+# Add the password to the Zabbix server configuration file
+echo "DBPassword=$pass" | sudo tee -a /etc/zabbix/zabbix_server.conf
+
+# Restart Zabbix server, agent, and Apache
+sudo systemctl restart zabbix-server zabbix-agent apache2
+
+# Enable automatic startup for Zabbix server, agent, and Apache
+sudo systemctl enable zabbix-server zabbix-agent apache2
+
+echo -e "\n=====================\nhttp://host/zabbix\nDB_NAME: zabbix_$rand\nDB_USER: user_$rand\nDB_PASSWORD: $pass\nAdmin/zabbix\n=====================\n"

ea982992bf0d9f335aa83bb9a4c9a1172702f5ff
Yurii Voitiuk <voutuk@icloud.com>
2023-09-27 11:32:09 +0300

    Create docker-install.sh

---
+++ docker-install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Set up error handling and exit on failure
+set -o errexit
+set -o nounset
+set -o pipefail
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo docker run hello-world

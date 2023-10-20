cb9e4e6add89db159c9c637559f59a9c8bd0beda
Yurii Voitiuk <voutuk@icloud.com>
2023-10-21 02:11:45 +0300

    Update docker-install.sh
    Updated the UI of the Bash script and added installation of Docker Compose.

--- docker-install.sh
+++ docker-install.sh
@@ -1,22 +1,80 @@
-#! /bin/bash
+#!/bin/bash
 
-# Set up error handling and exit on failure
-set -o errexit
-set -o nounset
-set -o pipefail
+error_handler() {
+    clear
+    echo -e "\n +-----------------------------------------------+"
+    echo -e " |                                               |"
+    echo -e " |   /\_/\     Script                            |"
+    echo -e " |  ( o-o )    execution                         |"
+    echo -e " |   > ~ <     error                             |"
+    echo -e " |                                               |"
+    echo -e " +-----------------------------------------------+"
+    exit 1
+}
 
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+trap 'error_handler' ERR
+
+clear
+echo -e "\n +-----------------------------------------------+"
+echo -e " |           Installation (Docker and more):     |"
+echo -e " |   /\_/\    - Install packages                 |"
+echo -e " |  ( o-o )   - Download GPG                     |"
+echo -e " |   > ~ <    - Add Docker to APT                |"
+echo -e " |            - Install Docker packages          |"
+echo -e " |           Installation is in progress         |"
+echo -e " +-----------------------------------------------+"
+
+sudo apt update > /dev/null 2>&1
+sudo apt install ca-certificates curl gnupg -y > /dev/null 2>&1
+
+clear
+echo -e "\n +-----------------------------------------------+"
+echo -e " |           Installation (Docker and more):     |"
+echo -e " |   /\_/\    + Install packages                 |"
+echo -e " |  ( o-o )   - Download GPG                     |"
+echo -e " |   > ~ <    - Add Docker to APT                |"
+echo -e " |            - Install Docker packages          |"
+echo -e " |           Installation is in progress         |"
+echo -e " +-----------------------------------------------+"
+
+sudo install -m 0755 -d /etc/apt/keyrings > /dev/null 2>&1
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg > /dev/null 2>&1
+sudo chmod a+r /etc/apt/keyrings/docker.gpg > /dev/null 2>&1
+
+clear
+echo -e "\n +-----------------------------------------------+"
+echo -e " |           Installation (Docker and more):     |"
+echo -e " |   /\_/\    + Install packages                 |"
+echo -e " |  ( o-o )   + Download GPG                     |"
+echo -e " |   > ~ <    - Add Docker to APT                |"
+echo -e " |            - Install Docker packages          |"
+echo -e " |           Installation is in progress         |"
+echo -e " +-----------------------------------------------+"
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update > /dev/null 2>&1
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+clear
+echo -e "\n +-----------------------------------------------+"
+echo -e " |           Installation (Docker and more):     |"
+echo -e " |   /\_/\    + Install packages                 |"
+echo -e " |  ( o-o )   + Download GPG                     |"
+echo -e " |   > ~ <    + Add Docker to APT                |"
+echo -e " |            - Install Docker packages          |"
+echo -e " |           Installation is in progress         |"
+echo -e " +-----------------------------------------------+"
 
-sudo docker run hello-world
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y > /dev/null 2>&1
+
+clear
+echo -e "\n +-----------------------------------------------+"
+echo -e " |           Installation (Docker and more):     |"
+echo -e " |   /\_/\    + Install packages                 |"
+echo -e " |  ( o-o )   + Download GPG                     |"
+echo -e " |   > ~ <    + Add Docker to APT                |"
+echo -e " |            + Install Docker packages          |"
+echo -e " |           Installation is successful          |"
+echo -e " +-----------------------------------------------+"

f233c264334488b89318949583dd77fa2961806f
Your Name <you@example.com>
2024-01-17 20:56:24 +0200

    del

--- zab-docker.sh
+++
@@ -1,4 +0,0 @@
-wget https://voutuk.github.io/bash/docker.sh && bash docker.sh
-git clone https://github.com/zabbix/zabbix-docker.git
-cd zabbix-docker/
-docker-compose -f ./docker-compose_v3_alpine_mysql_latest.yaml up -d

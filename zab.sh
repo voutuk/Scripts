be0856b75ebd6599546f0e5fc35895952cd7e7b9
Your Name <you@example.com>
2024-01-17 13:37:02 +0200

    add handle_error

--- zab.sh
+++ zab.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
 
-set -euo pipefail
+ubuntu_version=$(lsb_release -rs)
+handle_error() {
+    echo -e "\e[30m\e[41m ✘ An error occurred during execution. \e[0m"
+    rm zabbix-release_6.4-1+ubuntu${ubuntu_version}_all.deb
+    exit 1
+}
 
-ub=$(lsb_release -rs)
+mysql_error() {
+    echo -e "\e[30m\e[41m ✘ An error occurred during execution. Please try running the script again. \e[0m"
+    sudo mysql -uroot <<EOF
+DROP DATABASE IF EXISTS zabbix;
+DROP USER IF EXISTS 'zabbix'@'localhost';
+EOF
+    exit 1
+}
+trap 'handle_error' ERR
 
-echo -e "\e[30;44m ❍ Downloading and installing the Zabbix package \e[0m"
-wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu${ub}_all.deb
-sudo dpkg -i zabbix-release_6.4-1+ubuntu${ub}_all.deb
-rm zabbix-release_6.4-1+ubuntu${ub}_all.deb
-echo -e "\e[42m\e[30m ✔ Installing the Zabbix package \e[0m"
+echo -e "\e[30;44m ❍ Downloading and installing the Zabbix package. \e[0m"
+wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu${ubuntu_version}_all.deb
+sudo dpkg -i zabbix-release_6.4-1+ubuntu${ubuntu_version}_all.deb
+rm zabbix-release_6.4-1+ubuntu${ubuntu_version}_all.deb
+echo -e "\e[42m\e[30m ✔ Installing the Zabbix package. \e[0m"
 
-echo -e "\e[30;44m ❍ Installing Zabbix components \e[0m"
+echo -e "\e[30;44m ❍ Installing Zabbix components. \e[0m"
 sudo apt update
 sudo apt install zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-sql-scripts zabbix-agent mysql-server pv -y
 echo -e "\e[42m\e[30m ✔ Zabbix components \e[0m"
 
-echo -e "\e[30;44m ❍ Creating the Zabbix database \e[0m"
+echo -e "\e[30;44m ❍ Creating the Zabbix database. \e[0m"
 pass=$(openssl rand -base64 20)
-echo -e "\e[30;44m ✎ Enter root password \e[0m"
-sudo mysql -uroot -p <<EOF
+trap 'mysql_error' ERR
+sudo mysql -uroot <<EOF
 create database zabbix character set utf8mb4 collate utf8mb4_bin;
 create user zabbix@localhost identified by '$pass';
 grant all privileges on zabbix.* to zabbix@localhost;
 set global log_bin_trust_function_creators = 1;
 EOF
-echo -e "\e[42m\e[30m ✔ Zabbix database \e[0m"
+echo -e "\e[42m\e[30m ✔ Zabbix database. \e[0m"
 
-echo -e "\e[30;44m ❍ Decompressing a sql file \e[0m"
+echo -e "\e[30;44m ❍ Decompressing a sql file. \e[0m"
 zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | pv | mysql --default-character-set=utf8mb4 -uzabbix -p"$pass" zabbix
-echo -e "\e[42m\e[30m ✔ Decompressing a sql file \e[0m"
+echo -e "\e[42m\e[30m ✔ Decompressing a sql file. \e[0m"
 
-echo -e "\e[30;44m ✎ Enter root password \e[0m"
-mysql -uroot -p <<EOF
+mysql -uroot <<EOF
 set global log_bin_trust_function_creators = 0;
 EOF
 
-echo -e "\e[30;44m ❍ Writing the password to the configuration file \e[0m"
+echo -e "\e[30;44m ❍ Writing the password to the configuration file. \e[0m"
 echo "DBPassword=$pass" | sudo tee -a /etc/zabbix/zabbix_server.conf > /dev/null
 
 echo -e "\e[30;44m ❍ Starting services. \e[0m"
